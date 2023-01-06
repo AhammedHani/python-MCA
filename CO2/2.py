58fb495ffd8cbbdc90ff65e4c27c687ca9a1ca37
Ahammed Hani <105771459+AhammedHani@users.noreply.github.com>
2023-01-06 15:06:52 +0530

    CO2

---
+++ CO2/2.py
@@ -0,0 +1,12 @@
+def fibonacci(m):
+    n1=0
+    n2=1
+    count=0
+    if m<=0:
+        print("invalid")
+    elif m==1:
+        print("fibonacci series is")
+        print(n1)
+    else:
+        print("fibonacci series is")
+        while count<m
---
+++ CO2/3.py
@@ -0,0 +1,7 @@
+list=[]
+n=int(input("Enter the size of the list: "))
+print("Enter ",n, "elements of the list")
+for i in range(n):
+    a=int(input())
+    list.append(a)
+print("sum is:",sum(list))
